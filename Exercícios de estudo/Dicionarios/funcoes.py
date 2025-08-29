c1ab721d0fbd4b4c01ba89e85080e35ba302f662
fredericodepaula <freddepaula07@gmail.com>
2025-08-29 10:41:41 -0300

    manipulação de string

--- Exercícios de estudo/Dicionarios/funcoes.py
+++ Exercícios de estudo/Dicionarios/funcoes.py
@@ -7,3 +7,9 @@ def perguntar():
 def inserir_usuario(dicionario):
     dicionario[input("Digite o login o usuário: ").upper()] = [input("Digite o nome do usuário: ").upper() , 
                                                                  input("Digite a ultima data de acesso: ")]
+    salvar(dicionario)
+
+def salvar(dicionario):
+    with open("banco_de_dados.txt" "a") as arquivo:
+     for chave,valor in dicionario.items():
+        arquivo.write(chave + ":" + str(valor))
---
+++ texto.py
@@ -0,0 +1,4 @@
+texto = "Frederico e Valentina"
+
+print(texto[0:9])
+print(texto[12:])
